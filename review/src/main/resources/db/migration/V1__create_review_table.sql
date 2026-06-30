6c7983f7a235d71c509f10b79ceb190719af4c2a
Hanrol <be.cubillosp@duocuc.cl>
2026-06-30 11:56:23 -0400

    Arreglo .sql

--- review/src/main/resources/db/migration/V1__create_review_table.sql
+++ review/src/main/resources/db/migration/V1__create_review_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE reviews (
     review_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    destination_id BIGINT NOT NULL,
-    author_user_id BIGINT NOT NULL,
+    destination_id CHAR(36) NOT NULL,
+    author_user_id CHAR(36) NOT NULL,
     rating INT NOT NULL,
     comment VARCHAR(255),
     active BOOLEAN NOT NULL DEFAULT TRUE,
